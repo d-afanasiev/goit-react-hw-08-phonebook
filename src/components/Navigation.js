58989b8ed88fe6128044664593a0ffd1065b92cb
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-10-22 10:11:30 +0300

    change styles v.2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,19 +4,6 @@ import { useSelector } from "react-redux";
 import { authSelectors } from "../redux/auth";
 import css from "../styles/Navigation.module.css";
 
-// const styles = {
-//   link: {
-//     display: "inline-block",
-//     textDecoration: "none",
-//     padding: 12,
-//     fontWeight: 700,
-//     color: "#2A363B",
-//   },
-//   activeLink: {
-//     color: "#E84A5F",
-//   },
-// };
-
 const Navigation = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
